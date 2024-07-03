71352b0655780c0ad4c289c585192ab92ce79221
Lisa Wild <lisakwild@gmail.com>
2024-07-03 13:36:41 -0700

    feat: add counts for products and categories for admin/dashboard; make it look pretty

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @product_count = Product.count
+    @categorey_count = Category.count
   end
 
 end
