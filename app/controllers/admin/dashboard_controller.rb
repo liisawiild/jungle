63078df8efbc5d67f75c9dc658f8ac9f2c575df4
Lisa Wild <lisakwild@gmail.com>
2024-07-04 13:57:12 -0700

    refactor: correct spelling of categorey to category

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardController < ApplicationController
 
   def show
     @product_count = Product.count
-    @categorey_count = Category.count
+    @category_count = Category.count
   end
 
 end
