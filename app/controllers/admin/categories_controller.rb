52b3337a3eb114492d90999e91020dc166391f44
Lisa Wild <lisakwild@gmail.com>
2024-07-04 16:06:36 -0700

    feat: add a categories controller and page; link page elements to respective pages

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,13 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+  def index
+    @categories = Category.order("name")
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
