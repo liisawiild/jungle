f3a9e59d596710ecf2e4e013faae57ed0f96b305
Lisa Wild <lisakwild@gmail.com>
2024-07-18 14:33:06 -0700

    feat: add homepage testing and implement DatabaseCleaner in the config file

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,13 @@
+it("We visit the homepage", () => {
+  cy.visit("/");
+})
+
+it("There is products on the page", () => {
+  cy.visit("/");
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.visit("/");
+  cy.get(".products article").should("have.length", 2);
+})
